0427fefddc5a219bbf3a156379bfd9ffded3eb56
megat69 <>
2025-02-14 11:28:58 +0100

    Added ability to pass dash-args to each test binary

--- test.sh
+++ test.sh
@@ -6,14 +6,19 @@ NEUTRAL_COLOR='\033[0;33m'
 NO_COLOR='\033[0m'
 
 BIN_DIRECTORY="./build"
-TEST_ARGUMENTS=""  # Arguments to pass on to each test binary, such as -q, -s, or error-only
+TEST_ARGUMENTS=""  # Arguments to pass on to each test binary, such as -q, -s, -v, or -e
+
+if [ $# -ne 0 ] && [[ $1 == -* ]]; then
+    TEST_ARGUMENTS="$1"
+    shift
+fi
 
 if [ $# -eq 0 ]; then
-    ALL_EXECUTABLES_TEMP="$(find build/ -maxdepth 1 -type f -name "test_*")"
+    ALL_EXECUTABLES_TEMP="$(find "${BIN_DIRECTORY}" -maxdepth 1 -type f -name "test_*")"
     ALL_EXECUTABLES=""
     for EXECUTABLE in $ALL_EXECUTABLES_TEMP
     do
-        ALL_EXECUTABLES="${ALL_EXECUTABLES} ${EXECUTABLE#"${BIN_DIRECTORY#./}/test_"}"
+        ALL_EXECUTABLES="${ALL_EXECUTABLES} ${EXECUTABLE#"${BIN_DIRECTORY}/test_"}"
     done
 
     if [ -z "$ALL_EXECUTABLES" ]; then
@@ -27,9 +32,10 @@ fi
 if [ "$1" = "-h" ]; then
     echo -e "Runs through each provided test."
     echo -e "Tests are provided through command line arguments, being the name of any test executable, minus the 'test_' prefix.\n"
-    echo -e "Example : ./tests.sh tinytest1"
+    echo -e "Example : ./tests.sh [dash-args (-q|-v|-s|-e)] tinytest1"
     echo -e "\tWill run the executable '$BIN_DIRECTORY/test_tinytest1'"
     echo -e "If no name is provided, will run every test in the test directory."
+    echo -e "The dash-args (-q, -v, -s, -e) will be passed to each test, and correspond to TinyTest command line arguments."
     exit 0
 fi
 
